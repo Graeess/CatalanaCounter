b1a0d81bb5ccb40d6cee2a985c2b61593987d06a
Graeess <avargin_dmitrii_@mail.ru>
2025-05-12 10:48:44 +0600

    обавлены и обновлены .gitignore, build.yml

---
+++ src/main/java/catalana/catalanacounter/CatalanaCounterApplication.java
@@ -0,0 +1,13 @@
+package catalana.catalanacounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalanaCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalanaCounterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/catalana/catalanacounter/CatalanaCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package catalana.catalanacounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalanaCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
